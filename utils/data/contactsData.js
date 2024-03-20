ff02d5eaec50ba822c570a57737635cc79435acc
kejiantong <tong.ke@northeastern.edu>
2024-03-20 00:11:18 -0400

    redisign the website

---
+++ utils/data/contactsData.js
@@ -0,0 +1,7 @@
+export const contactsData = {
+  email: "tongcs2021@gmail.com",
+  address: "Seattle, WA ",
+  github: "https://github.com/kejian-tong",
+  linkedIn: "https://www.linkedin.com/in/tongoliver/",
+  devUsername: "olivertong",
+};
---
+++ utils/data/educations.js
@@ -0,0 +1,8 @@
+export const educations = [
+  {
+    id: 1,
+    title: "Master Degree in Computer Science",
+    duration: "2021 - 2023",
+    institution: "Northeastern University",
+  },
+];
